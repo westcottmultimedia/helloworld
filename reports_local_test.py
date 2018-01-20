088c6c1e6011b1fdeee581c1253ed2f10d68a58f
Eric Chen <eric.chen0121@gmail.com>
2018-01-19 18:02:54 -0800

    first pass on reporting in psql

---
+++ reports_local_test.py
@@ -0,0 +1,174 @@
+import psycopg2, csv, datetime, sys
+DATABASE_NAME = 'beats_backup'
+
+# sqlite database filename/path
+
+OUTPUT_FILE_TEMPLATE = './deliverables/client/psql_{}_{}_{}_{}.csv'
+CLIENT_NAME = 'bam'
+
+# NOTE: this should mirror the db table (ie. db table for bam client)
+SERVICE_MAP = {
+    1: 'spotify',
+    2: 'apple music'
+}
+
+SERVICE_CHARTS = {
+    1: 'track streaming',
+    2: 'track sales',
+    3: 'album sales',
+    4: 'music video sales'
+}
+
+class TrackDatabase(object):
+
+    def __init__(self):
+        super(TrackDatabase, self).__init__()
+        self.init_database()
+
+    def init_database(self):
+        # AWS
+        # rds_host  = "beats.cekfuk4kqawy.us-west-2.rds.amazonaws.com"
+        # name = "beatsdj"
+        # password = "beatsdj123"
+        # db_name = "beats"
+
+        # LOCAL
+        db_name ='beats_backup'
+        name= 'ericchen0121'
+        password = '123'
+
+        try:
+            # print('Connecting to the RDS PostgreSQL database {}...'.format(rds_host))
+            # AWS
+            # self.db = psycopg2.connect(host=rds_host, user=name, password=password, dbname=db_name)
+
+            # LOCAL BELOW Only
+            self.db = psycopg2.connect(user=name, password=password, dbname=db_name)
+            # print('Successfully connected to AWS RDS PostgreSQL instance.', rds_host)
+            self.db.autocommit = True
+            self.c = self.db.cursor()
+            self.show_db_size()
+        except Exception as e:
+            print(e)
+
+    def close_database(self):
+        try:
+            if self.db:
+                self.db.close()
+        except:
+            print('cannot close db')
+        return True
+
+    def show_db_size(self):
+        self.c.execute('SELECT pg_size_pretty(pg_database_size(current_database()))')
+        size = self.c.fetchone()[0]
+        print('Size: {}'.format(size))
+        return
+
+    # MATERIALIZED VIEWS
+    # http://www.postgresqltutorial.com/postgresql-materialized-views/
+
+    # TRACKS ADDED FROM YESTERDAY
+    #
+    def tp_add(self, date_to_process, refresh = false):
+
+        if refresh:
+            self.c.execute("""
+                REFRESH materialized VIEW tp_add
+            """
+            )
+        else:
+            self.c.execute("""
+                CREATE materialized VIEW tp_add as (
+                    WITH add_view AS (
+                        SELECT
+                            T1.date_str AS date_str,
+                            T1.id AS track_position_id,
+                            T1.service_id AS service_id,
+                            T1.territory_id AS territory_id,
+                            T1.track_id AS track_id,
+                            T1.isrc,
+                            T1.position AS today_position,
+                            CASE
+                                WHEN T2.position IS NULL THEN 'add'
+                                ELSE NULL
+                            END AS add_drop
+                        FROM track_position T1
+                        LEFT JOIN track_position T2
+                            ON T1.isrc = T2.isrc
+                            AND T1.territory_id = T2.territory_id
+                            AND T1.service_id = T2.service_id
+                            AND DATE(T1.date_str) - DATE(T2.date_str) = 1
+                        WHERE
+                            T1.date_str = '{}'
+                    )
+
+                    SELECT * FROM add_view
+                    WHERE add_drop = 'add'
+                )
+            """.format(date_to_process))
+
+    def tp_drop(self, date_to_process, refresh = false):
+        if refresh:
+            self.c.execute("""
+                REFRESH materialized VIEW tp_add
+            """
+            )
+        else:
+            self.c.execute("""
+                CREATE materialized VIEW tp_add as (
+                    WITH drop_view AS (
+                        SELECT
+                            T1.date_str as previous_date,
+                            T1.id as track_position_id,
+                            T1.service_id as service_id,
+                            T1.territory_id as territory_id,
+                            T1.track_id as track_id,
+                            T1.isrc,
+                            T1.position as previous_position,
+                            CASE
+                                WHEN T2.position is NULL then 'drop'
+                                ELSE NULL
+                            END AS add_drop
+                            FROM track_position T1
+                            LEFT JOIN track_position T2
+                                ON T1.isrc = T2.isrc
+                                AND T1.territory_id = T2.territory_id
+                                AND T1.service_id = T2.service_id
+                                AND DATE(T1.date_str) - DATE(T2.date_str) = -1
+                            WHERE
+                                DATE(T1.date_str) = date('{}') - 1
+                    )
+
+                    SELECT * FROM drop_view
+                    WHERE add_drop = 'drop'
+                )
+            """.format(date_to_process))
+
+
+if __name__ == '__main__':
+    db = TrackDatabase()
+
+    # previous
+    # conn = connect()
+    # NOW: db.db
+
+    # c = conn.cursor()
+    # NOW: db.c
+    #
+
+    # # spotify streaming
+    print('Generating Spotify stream reports')
+    service_id = 1
+    # print(latest_streamingDates)
+    # generateStreamingReporting(service_id, latest_streamingDates[service_id])
+    date_to_process = '2017-11-04'
+
+    # refresh_all = True
+
+    db.tp_add(date_to_process, refresh = false)
+    db.tp_drop(date_to_process, refresh = false)
+
+    # service_id, service_chart_id, table, query, columns = getStreamingTrackSpotifyQuery()
+    # exportReport(service_id, service_chart_id, table, query, columns)
+    # printDivider(40)
