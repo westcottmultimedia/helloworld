5b1fe3df2ea8c901ce8720c21ea9f950e2c0966d
Eric Chen <eric.chen0121@gmail.com>
2018-02-28 14:49:34 -0800

    genre percentages calculated

--- aws/genre_lambda.py
+++ aws/genre_lambda.py
@@ -89,6 +89,26 @@ class TrackDatabase(object):
             raise
             return None
 
+    def get_genres_from_artist_id(self, artist_id):
+        query = """
+            SELECT genre FROM artist_genre
+            WHERE artist_id = %s
+        """
+
+        try:
+            self.c.execute(query, [artist_id])
+
+            genres = []
+            for row in self.c.fetchall():
+                genres.append(row[0])
+                return genres
+
+        except Exception as e:
+            print('genre from artist_id error', e)
+            raise
+
+        return []
+
 class GenreRanks:
     def __init__(self, service, territory, kind, collection_type, date_str = 'latest'):
         self.db = TrackDatabase()
@@ -103,6 +123,7 @@ class GenreRanks:
         self.artist_ids = [] # list of ARTISTS in a collection of kinds (ie. stream list of songs, playlist of songs, sale list of music videos)
         self.genres = [] # list of all genres for self.artists
         self.genre_counts = {} # {genre: count, 'pop-rock': 5, ...} - counts of all genres for the list of artists
+        self.genre_percentages = {}
         self.date = date_str # can also be for a specific date
 
         self._map_db_tables()
@@ -143,8 +164,40 @@ class GenreRanks:
             artist_ids.append(self.db.get_artist_id_from_collection_id(self._kind_db_table, self._collection_db_table, collection_id))
 
         self.artist_ids = artist_ids
-        print(self.artist_ids)
         return True
+
+    def load_genres_ids(self):
+        genres = []
+        for artist_id in self.artist_ids:
+            genres.append(self.db.get_genres_from_artist_id(artist_id))
+
+        self.genres = genres
+        return True
+
+    def calculate_genre_counts(self):
+        genre_counts = {}
+        for group_of_genres in self.genres:
+            for genre in group_of_genres:
+                genre_counts[genre] = genre_counts.setdefault(genre, 0) + 1
+
+        self.genre_counts = genre_counts
+        return True
+
+    def calculate_genre_percentage(self):
+        total = 0
+        for genre, count in self.genre_counts.items():
+            total += count
+
+        genre_percentages = {}
+        for genre, count in self.genre_counts.items():
+            genre_percentages[genre] = count/total
+
+        self.genre_percentages = genre_percentages
+        return
+
+    # ranks: how many ranks to return? ie. ranks = 3 returns top 3 ranked genres
+    def get_top_genres(self, ranks = 3):
+        return sorted(self.genre_counts, reverse = True, key = self.genre_counts.__getitem__)[0:ranks]
 
 def test_handler(event, context):
         global db
@@ -154,6 +207,10 @@ def test_handler(event, context):
 
         gr.load_collection_ids()
         gr.load_artist_ids()
+        gr.load_genres_ids()
+        gr.calculate_genre_counts()
+        gr.calculate_genre_percentage()
+        gr.get_top_genres()
 
         db.close_database()
         print('closed database connection')
